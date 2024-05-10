2b39aaf409aec6e86aa3bcdbb9549f152f11b3f6
David Villen Moreno <dvillenmoreno@gmail.com>
2024-05-10 21:22:47 +0200

    finish v2

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 // const { i18n } = require('./next-i18next.config')
-
+const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   // i18n,
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  }
+  basePath: isProd ? '/uco-next' : '',
+  assetPrefix: isProd ? '../uco-next' : '',
 }
 
 //hs
